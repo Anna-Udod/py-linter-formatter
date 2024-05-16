b13b2caf6f1b6c8c1c536ccfa70add1c093a5686
Anna Udod <anna.rom18@gmail.com>
2024-05-16 09:09:23 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,8 +4,7 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8"
-    }
+        "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -19,5 +18,4 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 def format_linter_report(linter_report: dict) -> list:
     return [
         format_single_linter_file(file_path, errors)
-        for file_path, errors in linter_report.items()
-        ]
+        for file_path, errors in linter_report.items()]
